683dcbdbd567f757f00b1950e132fa2ac6c2ece8
mweaver20 <morganweaver123@yahoo.com>
2023-05-09 15:24:15 -0700

    adding nested routes + a redirect

--- src/app/App.js
+++ src/app/App.js
@@ -21,9 +21,13 @@ const router = createBrowserRouter( createRoutesFromElements(
     <Route path="sign-up" element={<SignUp/>} />
     <Route path="articles" element={<Articles/>} />
     <Route path="/articles/:title" element={<Article />} />
-    <Route path="categories" element={<Categories/>} />
-    <Route path="profile" element={<Profile/>} />
     <Route path="/authors/:name" element={<Author />} />
+    <Route path="categories" element={<Categories/>}>
+        <Route path=":name" element={<Category />} />
+    </Route>
+    <Route path="profile" element={<Profile/>}>
+      <Route path="edit"  element={ <EditProfileForm />} />
+    </Route> 
   </Route>
 ))
 function App() {
